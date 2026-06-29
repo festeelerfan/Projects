74520fbac3a3c8a18b5fac392b7afabc908119d4
festeelerfan <festeelerfan@gmail.com>
2026-06-29 14:38:50 -0400

    Greeks implemented

--- bs_pricer/src/pricing.py
+++ bs_pricer/src/pricing.py
@@ -1,15 +1,58 @@
 import math
 from scipy.stats import norm
+
+def _d1_d2(S: float, t: float, T: float, K: float, r: float, q: float, sigma: float) -> tuple[float, float]:
+   if S <= 0 or K <= 0 or sigma <= 0:
+      raise ValueError("S, K, and sigma must be positive")
+   if t < 0 or T < 0:
+      raise ValueError("t and T must be non-negative")
+   tau = T-t
+   d1 = 1/(sigma*math.sqrt(tau))*(math.log(S/K) + (r - q + 0.5*sigma**2)*(tau))
+   d2 = d1 - sigma*math.sqrt(tau)
+   return (d1, d2)
+
 def black_scholes (S: float, t: float, T: float, K: float, r: float, q: float, sigma: float) -> tuple[float, float]:
    if S <= 0 or K <= 0 or sigma <= 0:
       raise ValueError("S, K, and sigma must be positive")
    if t < 0 or T < 0:
       raise ValueError("t and T must be non-negative")
-   if T - t <= 0:
+   tau = T-t
+   if tau <= 0:
       return (max(S - K, 0), max(K - S, 0))
-   
-   d1 = 1/(sigma*math.sqrt(T-t))*(math.log(S/K) + (r - q + 0.5*sigma**2)*(T-t))
-   d2 = d1 - sigma*math.sqrt(T-t)
-   call = norm.cdf(d1)*S*math.exp(-q*(T-t)) - norm.cdf(d2)*K*math.exp(-r*(T-t))
-   put = norm.cdf(-d2)*K*math.exp(-r*(T-t)) - norm.cdf(-d1)*S*math.exp(-q*(T-t))
+
+   d1, d2 = _d1_d2(S, t, T, K, r, q, sigma)
+   call = S*math.exp(-q*tau)*norm.cdf(d1) - K*math.exp(-r*(T-t))*norm.cdf(d2)
+   put = K*math.exp(-r*tau)*norm.cdf(-d2) - S*math.exp(-q*(T-t))*norm.cdf(-d1)
+   return (call, put)
+
+def delta(S: float, t: float, T: float, K: float, r: float, q: float, sigma: float) -> tuple[float, float]:
+   d1 = _d1_d2(S, t, T, K, r, q, sigma)[0]
+   call = math.exp(-q*(T-t))*norm.cdf(d1)
+   put = math.exp(-q*(T-t))*(norm.cdf(d1)-1)
+   return (call, put)
+
+def gamma(S: float, t: float, T: float, K: float, r: float, q: float, sigma: float) -> float:
+   tau = T - t
+   d1 = _d1_d2(S, t, T, K, r, q, sigma)[0]
+   gamma = math.exp(-q*tau)/(S*sigma*math.sqrt(tau))*norm.pdf(d1)
+   return gamma
+
+def vega(S: float, t: float, T: float, K: float, r: float, q: float, sigma: float) -> float:
+   tau = T - t
+   d1 = _d1_d2(S, t, T, K, r, q, sigma)[0]
+   v = .01*S*math.exp(-q*tau)*math.sqrt(tau)*norm.pdf(d1)
+   return v
+
+def theta(S: float, t: float, T: float, K: float, r: float, q: float, sigma: float) -> tuple[float, float]:
+   tau = T - t
+   d1, d2 = _d1_d2(S, t, T, K, r, q, sigma)
+   call = -((S*sigma*math.exp(-q*tau))/(2*math.sqrt(tau))*norm.pdf(d1)) - r*K*math.exp(-r*tau)*norm.cdf(d2) + q*S*math.exp(-q*tau)*norm.cdf(d1)
+   put = -((S*sigma*math.exp(-q*tau))/(2*math.sqrt(tau))*norm.pdf(d1)) + r*K*math.exp(-r*tau)*norm.cdf(-d2) - q*S*math.exp(-q*tau)*norm.cdf(-d1)
+   return (call, put)
+
+def rho(S: float, t: float, T: float, K: float, r: float, q: float, sigma: float) -> tuple[float, float]:
+   tau = T - t
+   d2 = _d1_d2(S, t, T, K, r, q, sigma)[1]
+   call = .01*K*tau*math.exp(-r*tau)*norm.cdf(d2)
+   put = -.01*K*tau*math.exp(-r*tau)*norm.cdf(-d2)
    return (call, put)
--- bs_pricer/tests/test_pricing.py
+++ bs_pricer/tests/test_pricing.py
@@ -1,4 +1,4 @@
-from src.pricing import black_scholes
+from src.pricing import black_scholes, delta, gamma, vega, theta, rho
 
 def test_hull_example():
     call, put = black_scholes(S=42, t=0, T=0.5, K=40, r=0.1, q=0, sigma=0.2)
@@ -8,3 +8,26 @@ def test_zerodiv():
     call, put = black_scholes(S=1, t=0, T=0, K=2, r=0.1, q=0, sigma=0.2)
     assert call == max(1-2,0)
     assert put == max(2-1,0)
+
+def test_delta_hull_example():
+    call_delta, put_delta = delta(S=42, t=0, T=0.5, K=40, r=0.1, q=0, sigma=0.2)
+    assert abs(call_delta - 0.7791) < 0.01
+    assert abs(put_delta - (-0.2209)) < 0.01
+
+def test_gamma_hull_example():
+    g = gamma(S=42, t=0, T=0.5, K=40, r=0.1, q=0, sigma=0.2)
+    assert abs(g - 0.0500) < 0.01
+
+def test_vega_hull_example():
+    v = vega(S=42, t=0, T=0.5, K=40, r=0.1, q=0, sigma=0.2)
+    assert abs(v - 0.0881) < 0.01
+
+def test_theta_hull_example():
+    call_theta, put_theta = theta(S=42, t=0, T=0.5, K=40, r=0.1, q=0, sigma=0.2)
+    assert abs(call_theta - (-4.5591)) < 0.01
+    assert abs(put_theta - (-0.7542)) < 0.01
+
+def test_rho_hull_example():
+    call_rho, put_rho = rho(S=42, t=0, T=0.5, K=40, r=0.1, q=0, sigma=0.2)
+    assert abs(call_rho - 0.1398) < 0.01
+    assert abs(put_rho - (-0.0504)) < 0.01
